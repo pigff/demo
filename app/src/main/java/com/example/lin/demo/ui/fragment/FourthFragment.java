b115d4f18c857220c5de73d2eac08666d10a9c2e
lin <linjianlinhaha@163.com>
2016-11-21 13:34:19 +0800

    2016-11-21 fourth ok

--- app/src/main/java/com/example/lin/demo/ui/fragment/FourthFragment.java
+++ app/src/main/java/com/example/lin/demo/ui/fragment/FourthFragment.java
@@ -6,10 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import com.example.lin.demo.R;
+import com.example.lin.demo.adapter.CenterGridAdapter;
+import com.example.lin.demo.bean.Category;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FourthFragment extends Fragment {
+
+    private GridView mGridView;
+    private List<Category> mCategories;
+    private CenterGridAdapter mAdapter;
 
     public FourthFragment() {
 
@@ -30,8 +40,36 @@ public class FourthFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_fourth, container, false);
+        mGridView = (GridView) view.findViewById(R.id.center_grid);
+        initData();
+        initAdapter();
+        initView();
+        return view;
+    }
 
-        return inflater.inflate(R.layout.fragment_fourth, container, false);
+    private void initView() {
+        mGridView.setAdapter(mAdapter);
+    }
+
+    private void initAdapter() {
+        mAdapter = new CenterGridAdapter(getActivity(), mCategories);
+    }
+
+    private void initData() {
+        mCategories = new ArrayList<>();
+        Category category1 = new Category("头像设置", R.mipmap.img2);
+        Category category2 = new Category("修改密码", R.mipmap.img2);
+        Category category3 = new Category("运动数据", R.mipmap.img2);
+        Category category4 = new Category("健康记录", R.mipmap.img2);
+        Category category5 = new Category("健康检测", R.mipmap.img2);
+        Category category6 = new Category("保险查询", R.mipmap.img2);
+        mCategories.add(category1);
+        mCategories.add(category2);
+        mCategories.add(category3);
+        mCategories.add(category4);
+        mCategories.add(category5);
+        mCategories.add(category6);
     }
 
 }
